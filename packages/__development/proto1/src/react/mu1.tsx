9917afa1fadd3bf0cd3993463afc017ccfb140d9
yahiro <yahiro07@users.noreply.github.com>
2026-05-13 12:21:25 +0900

    add instrument unit

--- packages/__development/proto1/src/react/mu1.tsx
+++ packages/__development/proto1/src/react/mu1.tsx
@@ -1,10 +1,79 @@
 import "../styles/page.css";
 import "../styles/utility-classes.css";
-
 import { mountAppRoot } from "@wus/mo-react/mount-app-root";
+import { getHostInterface } from "@/common/unit-interfaces";
 
-const App = () => {
-  return <div className="w-dvw h-dvh bg-blue-100 p-2">mu1 sub app</div>;
-};
+const instrumentUnit = getHostInterface()?.createInstrumentUnit();
+const audioContext = instrumentUnit?.audioContext ?? new AudioContext();
+
+function midiToFrequency(midiNote: number): number {
+  return 440 * 2 ** ((midiNote - 69) / 12);
+}
+
+function createAppModel() {
+  const noteNodes: Record<number, OscillatorNode> = {};
+
+  return {
+    async noteOn(noteNumber: number) {
+      if (audioContext.state === "suspended") {
+        await audioContext.resume();
+      }
+      const freq = midiToFrequency(noteNumber);
+      const oscillatorNode = audioContext.createOscillator();
+      oscillatorNode.frequency.setValueAtTime(freq, audioContext.currentTime);
+      oscillatorNode.type = "sawtooth";
+      oscillatorNode.connect(audioContext.destination);
+      oscillatorNode.start();
+      noteNodes[noteNumber] = oscillatorNode;
+    },
+    noteOff(noteNumber: number) {
+      const oscillatorNode = noteNodes[noteNumber];
+      if (oscillatorNode) {
+        oscillatorNode.stop();
+        if (noteNodes[noteNumber]) {
+          delete noteNodes[noteNumber];
+        }
+      }
+    },
+  };
+}
+const appModel = createAppModel();
+
+function ToneButton(props: { label: string; noteNumber: number }) {
+  return (
+    <div
+      className="w-[60px] border border-gray-600 bg-gray-200 flex-c text-[40px] cursor-pointer"
+      onPointerDown={() => appModel.noteOn(props.noteNumber)}
+      onPointerUp={() => appModel.noteOff(props.noteNumber)}
+    >
+      {props.label}
+    </div>
+  );
+}
+
+function setupUnitInstance() {
+  instrumentUnit?.setup({
+    noteOn(noteNumber) {
+      appModel.noteOn(noteNumber);
+    },
+    noteOff(noteNumber) {
+      appModel.noteOff(noteNumber);
+    },
+  });
+}
+setupUnitInstance();
+
+function App() {
+  return (
+    <div className="w-dvw h-dvh flex-vc gap-4 bg-orange-200">
+      <div>mu1-instrument</div>
+      <div className="flex-c gap-4">
+        <ToneButton label="C" noteNumber={60} />
+        <ToneButton label="D" noteNumber={62} />
+        <ToneButton label="E" noteNumber={64} />
+      </div>
+    </div>
+  );
+}
 
 mountAppRoot(<App />);
