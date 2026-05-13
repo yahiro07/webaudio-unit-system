9f08fcff5fc67fcf6598be7a9d08b240cad0064b
yahiro <yahiro07@users.noreply.github.com>
2026-05-13 12:02:01 +0900

    apply web font

--- packages/__development/proto1/src/units/mu1-react.tsx
+++ packages/__development/proto1/src/units/mu1-react.tsx
@@ -4,7 +4,7 @@ import "../styles/utility-classes.css";
 import { mountAppRoot } from "@wus/mo-react/mount-app-root";
 
 const App = () => {
-  return <div className="w-dvw h-dvh bg-blue-100">mu1 sub app</div>;
+  return <div className="w-dvw h-dvh bg-blue-100 p-2">mu1 sub app</div>;
 };
 
 mountAppRoot(<App />);
--- packages/__development/proto1/src/units/mu2-solid.tsx
+++ packages/__development/proto1/src/units/mu2-solid.tsx
@@ -5,7 +5,7 @@ import "../styles/utility-classes.css";
 import { mountAppRoot } from "@wus/mo-solid/mount-app-root";
 
 const App = () => {
-  return <div class="w-dvw h-dvh bg-red-100">mu2 sub app</div>;
+  return <div class="w-dvw h-dvh bg-red-100 p-2">mu2 sub app</div>;
 };
 
 mountAppRoot(() => <App />);
