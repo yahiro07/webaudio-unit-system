6e9c1c761382e20b59913b0acd205de52ba8bebb
yahiro <yahiro07@users.noreply.github.com>
2026-05-15 07:02:47 +0900

    add copy script

---
+++ packages/__development/units-oss-js/build.sh
@@ -0,0 +1,6 @@
+dstDir=../units/oss-js
+rm -rf $dstDir
+mkdir -p $dstDir
+cp -r drum-machine/du10-drum-machine/web $dstDir/du10-drum-machine
+cp -r synthesizer/su11-webaudio-tinysynth/web $dstDir/su11-webaudio-tinysynth
+cp -r synthesizer/su12-wasyn-1/web $dstDir/su12-wasyn-1
