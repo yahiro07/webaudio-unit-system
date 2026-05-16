9698720b7d47c5ac7ce9f6bc4351481eb6233a81
yahiro <yahiro07@users.noreply.github.com>
2026-05-16 10:37:33 +0900

    change folder name

--- packages/__development/units-oss-js/build.sh
+++ packages/__development/units-oss-js/build.sh
@@ -4,4 +4,4 @@ mkdir -p $dstDir
 cp -r drum-machine/du10-drum-machine/web $dstDir/du10-drum-machine
 cp -r synthesizer/su11-webaudio-tinysynth/web $dstDir/su11-webaudio-tinysynth
 cp -r synthesizer/su12-wasyn-1/web $dstDir/su12-wasyn-1
-cp -r synthesizer/su20-webaudio-synth-v2-flat/web $dstDir/su20-webaudio-synth-v2-flat
+cp -r synthesizer/su20-webaudio-synth-v2/web $dstDir/su20-webaudio-synth-v2
