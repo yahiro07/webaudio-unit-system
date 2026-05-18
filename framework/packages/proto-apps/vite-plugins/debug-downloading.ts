95684c316383258b88404e76662d514b4a6bafd1
yahiro <yahiro07@users.noreply.github.com>
2026-05-18 17:58:38 +0900

    avoid lint error

--- framework/packages/proto-apps/vite-plugins/debug-downloading.ts
+++ framework/packages/proto-apps/vite-plugins/debug-downloading.ts
@@ -2,6 +2,7 @@ import { unitSourceUrls } from "../app2-solid/src/unit-source-urls";
 import { createRemoteUnitCacheStore } from "./remote-unit-cache-store";
 
 console.log("debug downloading");
+// biome-ignore lint/suspicious/noDebugger: debug
 debugger;
 
 const remoteUnitCacheStore = createRemoteUnitCacheStore("./.wus-unit-cache");
