38b81ad033c068d5c9a805f987ec34c28c333419
yahiro <yahiro07@users.noreply.github.com>
2026-05-18 13:40:46 +0900

    specify tag

--- framework/packages/proto-apps/app2-solid/src/unit-source-urls.ts
+++ framework/packages/proto-apps/app2-solid/src/unit-source-urls.ts
@@ -8,10 +8,10 @@ const wusCustomUnitsLocalRoot = new URL(
 // console.log({ repositoryRoot, wusCustomUnitsLocalRoot });
 
 export const unitSourceUrls_array = [
-  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@bundles/units/additive/",
-  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@bundles/units/drum-machine/",
-  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@bundles/units/wasyn-1/",
-  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@bundles/units/webaudio-tinysynth-simple/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r2/units/additive/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r2/units/drum-machine/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r2/units/wasyn-1/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r2/units/webaudio-tinysynth-simple/",
   // "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@bundles/units/koodori/",
   // "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@bundles/units/webaudio-synth-v2/",
   `${wusCustomUnitsLocalRoot}koodori/dist/`,
@@ -30,13 +30,13 @@ export const unitSourceUrls_array = [
 
 export const unitSourceUrls = {
   additive:
-    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@bundles/units/additive/",
+    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r2/units/additive/",
   drumMachine:
-    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@bundles/units/drum-machine/",
+    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r2/units/drum-machine/",
   wasyn1:
-    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@bundles/units/wasyn-1/",
+    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r2/units/wasyn-1/",
   webaudioTinysynthSimple:
-    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@bundles/units/webaudio-tinysynth-simple/",
+    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r2/units/webaudio-tinysynth-simple/",
   // "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@bundles/units/koodori/",
   // "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@bundles/units/webaudio-synth-v2/",
   koodori: `${wusCustomUnitsLocalRoot}koodori/dist/`,
