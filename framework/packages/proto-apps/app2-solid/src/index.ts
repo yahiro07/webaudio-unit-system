236a73591802a97200174e2ef428a88b28918039
yahiro <yahiro07@users.noreply.github.com>
2026-05-19 15:41:26 +0900

    check local units

--- framework/packages/proto-apps/app2-solid/src/index.ts
+++ framework/packages/proto-apps/app2-solid/src/index.ts
@@ -1,5 +1,5 @@
-import "./root";
-// import "./root2";
+// import "./root";
+import "./root2";
 // import "./x/dev0-external-unit";
 // import "./x/dev1-unit-check";
 // import "./x/dev2-load-units-by-page-url";
--- framework/packages/proto-apps/app2-solid/src/unit-source-urls.ts
+++ framework/packages/proto-apps/app2-solid/src/unit-source-urls.ts
@@ -21,41 +21,46 @@ export const unitSourceUrls_array = [
   // "https://cdn.jsdelivr.net/gh/yahiro07/webaudio-unit-system@load-remote-units/units/dist/dev/mu3-effect/",
   // "https://cdn.jsdelivr.net/gh/yahiro07/webaudio-unit-system@load-remote-units/units/dist/dev/mu4-keyboard/",
   // "https://cdn.jsdelivr.net/gh/yahiro07/webaudio-unit-system@load-remote-units/units/dist/dev/mu5-visualizer/",
-  `${repositoryRoot}units/units-dev/dist/mu1-instrument/`,
-  `${repositoryRoot}units/units-dev/dist/mu2-sequencer/`,
-  `${repositoryRoot}units/units-dev/dist/mu3-effect/`,
-  `${repositoryRoot}units/units-dev/dist/mu4-keyboard/`,
-  `${repositoryRoot}units/units-dev/dist/mu5-visualizer/`,
+  `${repositoryRoot}framework/packages/units-dev/dist/mu1-instrument/`,
+  `${repositoryRoot}framework/packages/units-dev/dist/mu2-sequencer/`,
+  `${repositoryRoot}framework/packages/units-dev/dist/mu3-effect/`,
+  `${repositoryRoot}framework/packages/units-dev/dist/mu4-keyboard/`,
+  `${repositoryRoot}framework/packages/units-dev/dist/mu5-visualizer/`,
 ];
 
 export const unitSourceUrls = {
-  additive:
-    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/additive/",
-  drumMachine:
-    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/drum-machine/",
-  wasyn1:
-    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/wasyn-1/",
-  webaudioTinysynthSimple:
-    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/webaudio-tinysynth-simple/",
-  bc010:
-    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/bc-010/",
-  koodori:
-    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/koodori/",
-  webaudioSynthV2:
-    "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/webaudio-synth-v2/",
-  // koodori: `${wusCustomUnitsLocalRoot}koodori/dist/`,
-  // webaudioSynthV2: `${wusCustomUnitsLocalRoot}webaudio-synth-v2/web/`,
-  // bc010: `${wusCustomUnitsLocalRoot}bc-010/build/`,
+  // additive:
+  //   "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/additive/",
+  // drumMachine:
+  //   "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/drum-machine/",
+  // wasyn1:
+  //   "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/wasyn-1/",
+  // webaudioTinysynthSimple:
+  //   "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/webaudio-tinysynth-simple/",
+  // bc010:
+  //   "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/bc-010/",
+  // koodori:
+  //   "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/koodori/",
+  // webaudioSynthV2:
+  //   "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r4/units/webaudio-synth-v2/",
+  additive: `${wusCustomUnitsLocalRoot}/dist/additive/`,
+  drumMachine: `${wusCustomUnitsLocalRoot}/dist/drum-machine/`,
+  wasyn1: `${wusCustomUnitsLocalRoot}/dist/wasyn-1/`,
+  webaudioTinysynthSimple: `${wusCustomUnitsLocalRoot}/dist/webaudio-tinysynth-simple/`,
+  bc010: `${wusCustomUnitsLocalRoot}/dist/bc-010/`,
+  koodori: `${wusCustomUnitsLocalRoot}/dist/koodori/`,
+  webaudioSynthV2: `${wusCustomUnitsLocalRoot}/dist/webaudio-synth-v2/`,
+
   // "https://cdn.jsdelivr.net/gh/yahiro07/webaudio-unit-system@load-remote-units/units/dist/dev/mu1-instrument/",
   // "https://cdn.jsdelivr.net/gh/yahiro07/webaudio-unit-system@load-remote-units/units/dist/dev/mu2-sequencer/",
   // "https://cdn.jsdelivr.net/gh/yahiro07/webaudio-unit-system@load-remote-units/units/dist/dev/mu3-effect/",
   // "https://cdn.jsdelivr.net/gh/yahiro07/webaudio-unit-system@load-remote-units/units/dist/dev/mu4-keyboard/",
   // "https://cdn.jsdelivr.net/gh/yahiro07/webaudio-unit-system@load-remote-units/units/dist/dev/mu5-visualizer/",
-  mu1Instrument: `${repositoryRoot}units/units-dev/dist/mu1-instrument/`,
-  mu2Sequencer: `${repositoryRoot}units/units-dev/dist/mu2-sequencer/`,
-  mu3Effect: `${repositoryRoot}units/units-dev/dist/mu3-effect/`,
-  mu4Keyboard: `${repositoryRoot}units/units-dev/dist/mu4-keyboard/`,
-  mu5Visualizer: `${repositoryRoot}units/units-dev/dist/mu5-visualizer/`,
+  mu1Instrument: `${repositoryRoot}framework/packages/units-dev/dist/mu1-instrument/`,
+  mu2Sequencer: `${repositoryRoot}framework/packages/units-dev/dist/mu2-sequencer/`,
+  mu3Effect: `${repositoryRoot}framework/packages/units-dev/dist/mu3-effect/`,
+  mu4Keyboard: `${repositoryRoot}framework/packages/units-dev/dist/mu4-keyboard/`,
+  mu5Visualizer: `${repositoryRoot}framework/packages/units-dev/dist/mu5-visualizer/`,
 };
 
 console.log("unit sources:", unitSourceUrls);
