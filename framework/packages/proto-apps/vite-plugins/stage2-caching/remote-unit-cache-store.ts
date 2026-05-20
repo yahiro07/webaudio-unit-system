b759b7355ca9665e8b9db4440f7f6029306505ed
yahiro <yahiro07@users.noreply.github.com>
2026-05-20 21:00:08 +0900

    cleanup code

--- framework/packages/proto-apps/vite-plugins/stage2-caching/remote-unit-cache-store.ts
+++ framework/packages/proto-apps/vite-plugins/stage2-caching/remote-unit-cache-store.ts
@@ -1,17 +1,6 @@
 import { ResolvedUnitEntry, UnitCacheEntry } from "../common/internal-types";
-import { UnitInventoriesJson, UnitSourceUrls } from "../common/types";
+import { UnitSourceUrls } from "../common/types";
 import { RemoteUnitCacheStorageIo } from "./remote-unit-cache-storage-io";
-
-export type RemoteUnitCacheStore = {
-  updateCachedContents(
-    unitSourceUrls: UnitSourceUrls,
-    resolvedUnitEntries: ResolvedUnitEntry[],
-  ): Promise<{
-    updated: boolean;
-    inventoriesJson: UnitInventoriesJson;
-  }>;
-  getUnitsCacheFolderPath(): string;
-};
 
 function checkDeepEquality(obj1: any, obj2: any): boolean {
   return JSON.stringify(obj1) === JSON.stringify(obj2);
@@ -56,26 +45,15 @@ export async function enumerateUnitEntriesToCache(
   );
 }
 
-// export function createRemoteUnitCacheStore(
-//   cacheFolderPath: string,
-// ): RemoteUnitCacheStore {
-//   return {
-//     async updateCachedContents(unitSourceUrls, resolvedUnitEntries) {},
-//     getUnitsCacheFolderPath() {
-//       return cacheStorageIo.getUnitsCacheFolderPath();
-//     },
-//   };
-// }
-
-type CacheCheckResult =
-  | { updating: false }
-  | { updating: true; unitEntriesToCache: UnitCacheEntry[] };
+type checkNeedRemoteUnitsCachingResult =
+  | { needUpdate: false }
+  | { needUpdate: true; unitEntriesToCache: UnitCacheEntry[] };
 
 export async function checkNeedRemoteUnitsCaching(
   cacheStorageIo: RemoteUnitCacheStorageIo,
   unitSourceUrls: UnitSourceUrls,
   resolvedUnitEntries: ResolvedUnitEntry[],
-): Promise<CacheCheckResult> {
+): Promise<checkNeedRemoteUnitsCachingResult> {
   const urlsChanged = await checkUnitSourceUrlsChanged(
     cacheStorageIo,
     unitSourceUrls,
@@ -85,7 +63,7 @@ export async function checkNeedRemoteUnitsCaching(
     cacheStorageIo,
   );
   if (!urlsChanged && unitEntriesToCache.length === 0) {
-    return { updating: false };
+    return { needUpdate: false };
   }
-  return { updating: true, unitEntriesToCache };
+  return { needUpdate: true, unitEntriesToCache };
 }
--- framework/packages/proto-apps/vite-plugins/unit-loader-plugin.ts
+++ framework/packages/proto-apps/vite-plugins/unit-loader-plugin.ts
@@ -1,6 +1,5 @@
 import { iife } from "@wus/ax/general-utils";
 import type { Plugin, ResolvedConfig } from "vite";
-import { unitSourceUrls } from "../app2-solid/src/unit-source-urls";
 import { ResolvedUnitEntry } from "./common/internal-types";
 import { UnitSourceUrls } from "./common/types";
 import {
@@ -9,10 +8,7 @@ import {
 } from "./common/unit-url-helpers";
 import { createResolvedUnitEntries } from "./stage1-input/unit-entry-resolver";
 import { formatUnitSourceUrlsToDictionary } from "./stage1-input/unit-source-urls-array-converter";
-import {
-  createRemoteUnitCacheStorageIo,
-  RemoteUnitCacheStorageIo,
-} from "./stage2-caching/remote-unit-cache-storage-io";
+import { createRemoteUnitCacheStorageIo } from "./stage2-caching/remote-unit-cache-storage-io";
 import { checkNeedRemoteUnitsCaching } from "./stage2-caching/remote-unit-cache-store";
 import { downloadUnitsFromRemote } from "./stage2-caching/remote-units-downloader";
 import {
@@ -22,69 +18,6 @@ import {
 import { createDevServerMiddleware } from "./stage4-dev-serving/dev-server-middleware";
 import { writeBundleImpl } from "./stage5-build/write-bundle-impl";
 
-const startingFlow = {
-  processStage1(
-    cacheStorageIo: RemoteUnitCacheStorageIo,
-    unistSourceUrlsInput: UnitSourceUrls | string[],
-  ) {
-    const unitSourceUrls =
-      formatUnitSourceUrlsToDictionary(unistSourceUrlsInput);
-    Object.values(unitSourceUrls).forEach(checkUnitSourceUrlFormat);
-    const unitsCacheFolderPath = cacheStorageIo.getUnitsCacheFolderPath();
-    const resolvedUnitEntries = createResolvedUnitEntries(
-      unitSourceUrls,
-      unitsCacheFolderPath,
-    );
-    return [unitSourceUrls, resolvedUnitEntries] as const;
-  },
-  async processStage2(
-    cacheStorageIo: RemoteUnitCacheStorageIo,
-    unitSourceUrls: UnitSourceUrls,
-    resolvedUnitEntries: ResolvedUnitEntry[],
-    cacheFolderPath: string,
-  ): Promise<boolean> {
-    const res = await checkNeedRemoteUnitsCaching(
-      cacheStorageIo,
-      unitSourceUrls,
-      resolvedUnitEntries,
-    );
-    if (res.needUpdate) {
-      await downloadUnitsFromRemote(
-        res.unitEntriesToCache,
-        cacheStorageIo.writeCachedPiece,
-        cacheFolderPath,
-      );
-      return true;
-    }
-    return false;
-  },
-  async processStage3(
-    cacheStorageIo: RemoteUnitCacheStorageIo,
-    resolvedUnitEntries: ResolvedUnitEntry[],
-    summaryOutputPath: string,
-    hasUpdated: boolean,
-  ) {
-    const inventoriesJson = await iife(async () => {
-      const cachedInventoriesJson =
-        await cacheStorageIo.readCachedSummariesJson();
-
-      if (!hasUpdated && cachedInventoriesJson) {
-        return cachedInventoriesJson;
-      } else {
-        const inventoriesJson =
-          await generateSummariesJson(resolvedUnitEntries);
-        await cacheStorageIo.writePreviousUnitSourceUrlsInput(unitSourceUrls);
-        await cacheStorageIo.writeCachedSummariesJson(inventoriesJson);
-        return inventoriesJson;
-      }
-    });
-    const summaryFileExists = await checkFileExists(summaryOutputPath);
-    if (hasUpdated || !summaryFileExists) {
-      writeSummariesJsonToFile(inventoriesJson, summaryOutputPath);
-    }
-  },
-};
-
 export function unitLoaderPlugin(options: {
   unitSourceUrls: UnitSourceUrls | string[];
   cacheFolderPath?: string;
@@ -93,10 +26,62 @@ export function unitLoaderPlugin(options: {
   const cacheFolderPath = options.cacheFolderPath ?? "~/.wus/cache";
   const summaryOutputPath =
     options.summaryOutputPath ?? "src/unit-inventories.json";
-  const unistSourceUrlsInput = options.unitSourceUrls;
   let config: ResolvedConfig;
+  let unitSourceUrls: UnitSourceUrls;
   let resolvedUnitEntries: ResolvedUnitEntry[];
   const cacheStorageIo = createRemoteUnitCacheStorageIo(cacheFolderPath);
+
+  const startingFlow = {
+    processStage1() {
+      //stage1 - resolve unit entires
+      unitSourceUrls = formatUnitSourceUrlsToDictionary(options.unitSourceUrls);
+      Object.values(unitSourceUrls).forEach(checkUnitSourceUrlFormat);
+      const unitsCacheFolderPath = cacheStorageIo.getUnitsCacheFolderPath();
+      resolvedUnitEntries = createResolvedUnitEntries(
+        unitSourceUrls,
+        unitsCacheFolderPath,
+      );
+      console.log(resolvedUnitEntries);
+    },
+    async processStage2() {
+      //stage2 - check and download remote units if needed
+      const res = await checkNeedRemoteUnitsCaching(
+        cacheStorageIo,
+        unitSourceUrls,
+        resolvedUnitEntries,
+      );
+      if (res.needUpdate) {
+        await downloadUnitsFromRemote(
+          res.unitEntriesToCache,
+          cacheStorageIo.writeCachedPiece,
+          cacheFolderPath,
+        );
+        return true;
+      }
+      return false;
+    },
+    async processStage3(hasUpdated: boolean) {
+      //stage3 - generate summaries json and write to file if needed
+      const inventoriesJson = await iife(async () => {
+        const cachedInventoriesJson =
+          await cacheStorageIo.readCachedSummariesJson();
+
+        if (!hasUpdated && cachedInventoriesJson) {
+          return cachedInventoriesJson;
+        } else {
+          const inventoriesJson =
+            await generateSummariesJson(resolvedUnitEntries);
+          await cacheStorageIo.writePreviousUnitSourceUrlsInput(unitSourceUrls);
+          await cacheStorageIo.writeCachedSummariesJson(inventoriesJson);
+          return inventoriesJson;
+        }
+      });
+      const summaryFileExists = await checkFileExists(summaryOutputPath);
+      if (hasUpdated || !summaryFileExists) {
+        writeSummariesJsonToFile(inventoriesJson, summaryOutputPath);
+      }
+    },
+  };
 
   return {
     name: "unit-loader",
@@ -104,29 +89,9 @@ export function unitLoaderPlugin(options: {
       config = _config;
     },
     async buildStart() {
-      //stage1 - resolve unit entires
-      let unitSourceUrls: UnitSourceUrls;
-      [unitSourceUrls, resolvedUnitEntries] = startingFlow.processStage1(
-        cacheStorageIo,
-        unistSourceUrlsInput,
-      );
-      console.log(resolvedUnitEntries);
-
-      //stage2 - check and download remote units if needed
-      const hasUpdated = await startingFlow.processStage2(
-        cacheStorageIo,
-        unitSourceUrls,
-        resolvedUnitEntries,
-        cacheFolderPath,
-      );
-
-      //stage3 - generate summaries json and write to file if needed
-      await startingFlow.processStage3(
-        cacheStorageIo,
-        resolvedUnitEntries,
-        summaryOutputPath,
-        hasUpdated,
-      );
+      startingFlow.processStage1();
+      const hasUpdated = await startingFlow.processStage2();
+      await startingFlow.processStage3(hasUpdated);
     },
     configureServer(server) {
       const middleware = createDevServerMiddleware(resolvedUnitEntries);
