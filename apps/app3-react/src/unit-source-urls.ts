8b88f4e5cfe737c8f6dfa6c25df18cde6086e49f
yahiro <yahiro07@users.noreply.github.com>
2026-05-29 10:48:13 +0900

    add development project

---
+++ apps/app3-react/src/unit-source-urls.ts
@@ -0,0 +1,28 @@
+const homeDir = process.env.HOME;
+
+export const unitSourceUrls = [
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-units@r6/wavicle/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-units@r6/specbar/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-units@r6/proto-engine-ptm-osc/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-units@r6/proto-engine-pd-fm/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-units@r6/mini-synth/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-units@r6/mini-synth-ge/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-units@r6/mini-synth-gp/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-units@r6/useq/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-units@r6/lseq1/",
+
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r8/additive/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r8/drum-machine/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r8/wasyn-1/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r8/webaudio-tinysynth-simple/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r8/bc-010/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r8/koodori/",
+  "https://cdn.jsdelivr.net/gh/yahiro07/wus-custom-units@r8/webaudio-synth-v2/",
+  // `/dev-units/mu1-instrument/`,
+  // `/dev-units/mu2-sequencer/`,
+  // `/dev-units/mu3-effect/`,
+  // `/dev-units/mu4-keyboard/`,
+  // `/dev-units/mu5-visualizer/`,
+  `file://${homeDir}/wus-my-units/my-drum-machine/`,
+  `file://${homeDir}/wus-my-units/twsq1/`,
+];
