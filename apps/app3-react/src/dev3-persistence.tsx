47af5d1576c0515d45d0278b8e8bbc3de8200e21
yahiro <yahiro07@users.noreply.github.com>
2026-05-29 14:15:47 +0900

    save unit states

--- apps/app3-react/src/dev3-persistence.tsx
+++ apps/app3-react/src/dev3-persistence.tsx
@@ -5,6 +5,20 @@ import { createHostSystem } from "@/host-system/host";
 import { UnitFrame } from "@/host-system/react";
 import catalog from "./unit-inventories.json";
 
+function loadSavedData(): ProjectData | undefined {
+  const text = localStorage.getItem(`dev3-persistence-saved-data`);
+  if (text) {
+    try {
+      const data = JSON.parse(text) as ProjectData;
+      return data;
+    } catch (e) {
+      console.warn(`Failed to parse saved data:`, e);
+      return undefined;
+    }
+  }
+}
+const savedData = loadSavedData();
+
 type CatalogKey = keyof typeof catalog;
 
 type StoreState = {
@@ -14,14 +28,70 @@ type StoreState = {
 const audioContext = new AudioContext();
 const hostSystem = createHostSystem(audioContext);
 const store = createStore<StoreState>({
-  catalogKey: Object.keys(catalog)[0] as CatalogKey,
+  catalogKey: savedData?.catalogKey ?? "mini_synth",
 });
+
+type UnitStateData =
+  | { type: "bytes"; base64: string }
+  | { type: "json"; json: any };
+
+function isUint8ArrayLike(value: unknown): value is Uint8Array {
+  return (
+    value instanceof Uint8Array ||
+    (ArrayBuffer.isView(value) &&
+      Object.prototype.toString.call(value) === "[object Uint8Array]")
+  );
+}
+
+function mapUnitStateToData(
+  state: Uint8Array | Record<string, any>,
+): UnitStateData | undefined {
+  if (isUint8ArrayLike(state)) {
+    const base64 = btoa(String.fromCharCode(...state));
+    return { type: "bytes", base64 };
+  } else if (state && typeof state === "object") {
+    return { type: "json", json: state };
+  }
+}
+
+type ProjectData = {
+  catalogKey: CatalogKey;
+  unitStates: {
+    id: string;
+    data?: UnitStateData;
+  }[];
+};
+
+const actions = {
+  saveSceneStates() {
+    const { catalogKey } = store.state;
+    const units = hostSystem.getUnits();
+    const states = units.map((unit) => {
+      const state =
+        unit.persistence?.emitStateBytes?.() ?? unit.persistence?.emitState?.();
+      console.log({ state });
+      return {
+        id: unit.unitId,
+        data: state ? mapUnitStateToData(state) : undefined,
+      };
+    });
+    const projectData: ProjectData = {
+      catalogKey,
+      unitStates: states,
+    };
+    localStorage.setItem(
+      `dev3-persistence-saved-data`,
+      JSON.stringify(projectData),
+    );
+  },
+};
 
 const PageRoot = () => {
   const { catalogKey } = store.useSnapshot();
 
-  const instrumentUnitId = `uf_instrument_${catalogKey}`;
-  const keyboardUnitId = `uf_keyboard_${catalogKey}`;
+  const visualizerUnitId = `uf_visualizer`;
+  const instrumentUnitId = `uf_instrument:${catalogKey}`;
+  const keyboardUnitId = `uf_keyboard:${catalogKey}`;
   return (
     <div className="w-dvw h-dvh flex-c">
       <div className="flex-v gap-6 w-[1000px]">
@@ -39,12 +109,12 @@ const PageRoot = () => {
           </select>
           <div className="flex-ha gap-4">
             <Button text="reload page" />
-            <Button text="save states" />
+            <Button text="save states" onClick={actions.saveSceneStates} />
           </div>
         </div>
         <div className="border border-gray-400 flex-vc h-[700px]">
           <UnitFrame
-            unitId="uf_visualizer"
+            unitId={visualizerUnitId}
             destUnitId="$output"
             pageUrl={catalog.specbar.loaderPageUrl}
             frameSize={catalog.specbar.preferredSize}
@@ -53,7 +123,7 @@ const PageRoot = () => {
           <UnitFrame
             key={instrumentUnitId}
             unitId={instrumentUnitId}
-            destUnitId="uf_visualizer"
+            destUnitId={visualizerUnitId}
             pageUrl={catalog[catalogKey].loaderPageUrl}
             frameSize={catalog[catalogKey].preferredSize}
             hostSystem={hostSystem}
