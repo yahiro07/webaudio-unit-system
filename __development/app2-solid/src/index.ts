2382efac65b149c3e72fc48170de5acc77ee7c51
yahiro <yahiro07@users.noreply.github.com>
2026-06-06 00:05:39 +0900

    adjust configs

--- __development/app2-solid/src/index.ts
+++ __development/app2-solid/src/index.ts
@@ -1,8 +1,8 @@
 import "./page.css";
 import "mofus/ax-ui/utility-classes.css";
 //
-import "./root";
-// import "./root2";
+// import "./root";
+import "./root2";
 // import "./x/dev0-external-unit";
 // import "./x/dev1-unit-check";
 // import "./x/dev2-load-units-by-page-url";
