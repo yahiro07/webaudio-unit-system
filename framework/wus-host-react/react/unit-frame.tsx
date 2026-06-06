b4866f6e16c875d005d57f3dd4cc5ac703dfd791
yahiro <yahiro07@users.noreply.github.com>
2026-06-06 12:18:30 +0900

    add iframe callback

--- framework/wus-host-react/react/unit-frame.tsx
+++ framework/wus-host-react/react/unit-frame.tsx
@@ -9,23 +9,24 @@ type Props = {
   unitId: string;
   pageUrl: string;
   destSpec?: string;
-  loadedCallback?(unitInstance: HsUnitInstance): void;
   className?: string;
   style?: CSSProperties;
   frameSize?: { width: number; height: number };
-  // onIframeMounted?(iframe: HTMLIFrameElement): (() => void) | undefined;
   inputNotes?: number[];
+  onIframeMounted?(iframe: HTMLIFrameElement): (() => void) | undefined;
+  onUnitInstanceLoaded?(unitInstance: HsUnitInstance): void;
 };
 
 export const UnitFrame = ({
   unitId,
   pageUrl,
   destSpec,
-  loadedCallback,
   className,
   style,
   frameSize,
   inputNotes,
+  onIframeMounted,
+  onUnitInstanceLoaded,
 }: Props) => {
   const iframeRef = useRef<HTMLIFrameElement>(null);
   const unitInstanceRef = useRef<HsUnitInstance>(null);
@@ -44,6 +45,8 @@ export const UnitFrame = ({
   // biome-ignore lint/correctness/useExhaustiveDependencies: add pageUrl to deps
   useEffect(() => {
     const iframe = iframeRef.current!;
+    const cleanupIFrameCallback = onIframeMounted?.(iframe);
+
     const win = iframe.contentWindow;
     const unitInstantiationPromise = new Promise<HsUnitInstance>((resolve) => {
       (win as any).unitInterface = createUnitInterface(
@@ -51,16 +54,20 @@ export const UnitFrame = ({
         unitId,
         (unitInstance) => {
           // console.log(`unit loaded for ${unitId}`);
-          loadedCallback?.(unitInstance);
+          onUnitInstanceLoaded?.(unitInstance);
           unitInstanceRef.current = unitInstance;
           resolve(unitInstance);
         },
       );
     });
-    return hostSystem.registerPendingUnitInstancePromise(
+    const unregisterUnit = hostSystem.registerPendingUnitInstancePromise(
       unitId,
       unitInstantiationPromise,
     );
+    return () => {
+      unregisterUnit();
+      cleanupIFrameCallback?.();
+    };
   }, [pageUrl, hostSystem]);
 
   useEffect(() => {
