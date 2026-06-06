55659f384d97837037b6517d139170884edd281d
yahiro <yahiro07@users.noreply.github.com>
2026-06-06 12:02:19 +0900

    add host app context

---
+++ framework/wus-host-react/react/host-app-context.tsx
@@ -0,0 +1,79 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useMemo,
+} from "react";
+import { HostSystem } from "../host";
+import { createSequencerTickDriver } from "../host/sequencer-tick-driver/sequencer-tick-driver";
+
+type HostAppContextValue = {
+  hostSystem: HostSystem;
+  playing?: boolean;
+  bpm?: number;
+  masterGain?: number;
+};
+
+const hostAppContext = createContext<HostAppContextValue>(undefined!);
+
+export function useHostAppContext() {
+  return useContext(hostAppContext);
+}
+
+function useHostAppDrivers({
+  hostSystem,
+  playing = false,
+  bpm,
+  masterGain,
+}: {
+  hostSystem: HostSystem;
+  playing?: boolean;
+  bpm?: number;
+  masterGain?: number;
+}) {
+  const sequencerTickDriver = useMemo(
+    () => createSequencerTickDriver(hostSystem),
+    [hostSystem],
+  );
+  // useEffect(hostSystem.setupLifecycle, []);
+  useEffect(() => {
+    if (masterGain !== undefined) {
+      hostSystem.setMasterGain(masterGain);
+    }
+  }, [hostSystem, masterGain]);
+  useEffect(() => {
+    if (bpm) {
+      sequencerTickDriver.setBpm(bpm);
+    }
+  }, [sequencerTickDriver, bpm]);
+  useEffect(() => {
+    if (playing) {
+      sequencerTickDriver.start();
+      return () => sequencerTickDriver.stop();
+    } else {
+      sequencerTickDriver.stop();
+    }
+  }, [sequencerTickDriver, playing]);
+}
+
+export const HostAppProvider = ({
+  hostSystem,
+  playing = false,
+  bpm,
+  masterGain,
+  children,
+}: {
+  hostSystem: HostSystem;
+  playing?: boolean;
+  bpm?: number;
+  masterGain?: number;
+  children: ReactNode;
+}) => {
+  useHostAppDrivers({ hostSystem, playing, bpm, masterGain });
+  return (
+    <hostAppContext.Provider value={{ hostSystem, playing, bpm, masterGain }}>
+      {children}
+    </hostAppContext.Provider>
+  );
+};
