c95cc25c7ea9b0ede34b5fc7d082c3afa738e2aa
yahiro <yahiro07@users.noreply.github.com>
2026-06-06 22:14:41 +0900

    add compatibility checking function

--- framework/wus-host/react/iframe-unit-loader.ts
+++ framework/wus-host/react/iframe-unit-loader.ts
@@ -1,6 +1,6 @@
+import { WindowWithUnitInterface } from "wus-unit-types";
 import { HostSystem, HsUnitInstance } from "../host";
 import { createUnitInterface } from "../host/unit-interface-impl";
-
 export function loadIframeUnitInstance(
   hostSystem: HostSystem,
   unitId: string,
@@ -12,9 +12,17 @@ export function loadIframeUnitInstance(
   },
 ) {
   const cleanupIFrameCallback = sideEffects.onIframeMounted?.(iframe);
-  const win = iframe.contentWindow;
+  const win = iframe.contentWindow as WindowWithUnitInterface;
   const unitInstantiationPromise = new Promise<HsUnitInstance>((resolve) => {
-    (win as any).unitInterface = createUnitInterface(
+    win.checkUnitInterfaceCompatibility = (versionCode: string) => {
+      if (versionCode !== "wus-v02") {
+        console.warn(
+          `incompatible unit interface version: ${versionCode} for ${unitId}`,
+        );
+        win.unitInterface = undefined;
+      }
+    };
+    win.unitInterface = createUnitInterface(
       hostSystem.audioContext,
       unitId,
       (unitInstance) => {
