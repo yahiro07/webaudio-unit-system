e0188f8275aaf149b34bf594d2cf4b70f3289e1b
yahiro <yahiro07@users.noreply.github.com>
2026-06-11 08:41:22 +0900

    separate function

--- framework/wus-host/react/custom-element-unit-frame.tsx
+++ framework/wus-host/react/custom-element-unit-frame.tsx
@@ -1,4 +1,6 @@
 import { CSSProperties, useEffect, useMemo, useRef } from "react";
+import { UnitInstantiateContext, UnitInterface } from "wus-unit-types";
+import { HostSystem } from "../host";
 import { HsUnitInstance } from "../host/host-types";
 import { createUnitInterface } from "../host/unit-interface-impl";
 import { mergeStyleWithFrameSize } from "../utils/frame-size-helper";
@@ -16,6 +18,51 @@ type Props = {
   inputNotes?: number[];
   onUnitInstanceLoaded?(unitInstance: HsUnitInstance): void;
 };
+
+function createUnitInstantiationPromise(
+  unitId: string,
+  scriptUrl: string,
+  hostSystem: HostSystem,
+  callbacks: {
+    onElementCreated: (element: HTMLElement) => void;
+    onInstanceLoaded: (unitInstance: HsUnitInstance) => void;
+  },
+) {
+  return new Promise<HsUnitInstance>(
+    // biome-ignore lint/suspicious/noAsyncPromiseExecutor: rough impl
+    async (resolve) => {
+      const tagName = `unit-${Math.random().toString().slice(2, 8)}`;
+
+      await loadUnitElementClassCached(tagName, scriptUrl);
+
+      const element = document.createElement(tagName);
+
+      let unitInterface: UnitInterface | undefined = createUnitInterface(
+        hostSystem.audioContext,
+        unitId,
+        (unitInstance) => {
+          callbacks.onInstanceLoaded(unitInstance);
+          resolve(unitInstance);
+        },
+      );
+      const unitInstantiateContext: UnitInstantiateContext = {
+        checkUnitInterfaceCompatibility(versionCode: string) {
+          if (versionCode !== "wus-v02") {
+            console.warn(
+              `incompatible unit interface version: ${versionCode} for ${unitId}`,
+            );
+            unitInterface = undefined;
+          }
+        },
+        get unitInterface() {
+          return unitInterface;
+        },
+      };
+      (element as any).setupUnit(unitInstantiateContext);
+      callbacks.onElementCreated(element);
+    },
+  );
+}
 
 export const CustomElementUnitFrame = ({
   unitId,
@@ -46,36 +93,19 @@ export const CustomElementUnitFrame = ({
       const container = containerRef.current;
       let createdElement: HTMLElement | undefined;
 
-      const unitInstantiationPromise = new Promise<HsUnitInstance>(
-        // biome-ignore lint/suspicious/noAsyncPromiseExecutor: rough impl
-        async (resolve) => {
-          const tagName = `unit-${Math.random().toString().slice(2, 8)}`;
-
-          await loadUnitElementClassCached(tagName, scriptUrl);
-
-          const element = document.createElement(tagName);
-
-          const setupArgs: any = {};
-          setupArgs.checkUnitInterfaceCompatibility = (versionCode: string) => {
-            if (versionCode !== "wus-v02") {
-              console.warn(
-                `incompatible unit interface version: ${versionCode} for ${unitId}`,
-              );
-              setupArgs.unitInterface = undefined;
-            }
-          };
-          setupArgs.unitInterface = createUnitInterface(
-            hostSystem.audioContext,
-            unitId,
-            (unitInstance) => {
-              unitInstanceRef.current = unitInstance;
-              onUnitInstanceLoaded?.(unitInstance);
-              resolve(unitInstance);
-            },
-          );
-          (element as any).setupUnit(setupArgs);
-          createdElement = element;
-          container.appendChild(element);
+      const unitInstantiationPromise = createUnitInstantiationPromise(
+        unitId,
+        scriptUrl,
+        hostSystem,
+        {
+          onElementCreated(element) {
+            createdElement = element;
+            container.appendChild(element);
+          },
+          onInstanceLoaded(instance) {
+            unitInstanceRef.current = instance;
+            onUnitInstanceLoaded?.(instance);
+          },
         },
       );
       const unregisterUnit = hostSystem.registerPendingUnitInstancePromise(
