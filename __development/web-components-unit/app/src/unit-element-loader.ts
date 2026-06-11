9c70937f7a197839374da9323087f8be88e34de1
yahiro <yahiro07@users.noreply.github.com>
2026-06-11 18:45:11 +0900

    update development code

--- __development/web-components-unit/app/src/unit-element-loader.ts
+++ __development/web-components-unit/app/src/unit-element-loader.ts
@@ -1,20 +1,27 @@
 export async function loadUnitElement(tagName: string, moduleUrl: string) {
-  const unit1ModuleText = await fetch(moduleUrl).then((response) => {
-    if (!response.ok) {
-      throw new Error(`Failed to fetch ${moduleUrl}: ${response.status}`);
-    }
-    return response.text();
-  });
+  if (0) {
+    const unit1ModuleText = await fetch(moduleUrl).then((response) => {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch ${moduleUrl}: ${response.status}`);
+      }
+      return response.text();
+    });
 
-  const unit1ModuleBlobUrl = URL.createObjectURL(
-    new Blob([unit1ModuleText], { type: "text/javascript" }),
-  );
+    const unit1ModuleBlobUrl = URL.createObjectURL(
+      new Blob([unit1ModuleText], { type: "text/javascript" }),
+    );
 
-  const unitElement = (await import(/* @vite-ignore */ unit1ModuleBlobUrl).then(
-    (module) => module.default,
-  )) as any;
+    const unitElement = (await import(
+      /* @vite-ignore */ unit1ModuleBlobUrl
+    ).then((module) => module.default)) as any;
 
-  URL.revokeObjectURL(unit1ModuleBlobUrl);
+    URL.revokeObjectURL(unit1ModuleBlobUrl);
 
-  customElements.define(tagName, unitElement);
+    customElements.define(tagName, unitElement);
+  } else {
+    const unitElement = (await import(moduleUrl).then(
+      (module) => module.default,
+    )) as any;
+    customElements.define(tagName, unitElement);
+  }
 }
