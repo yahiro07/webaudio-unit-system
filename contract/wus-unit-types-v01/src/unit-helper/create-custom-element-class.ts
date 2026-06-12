387ea829f7c59a274b8a94bf04b3abcbeef19354
yahiro <yahiro07@users.noreply.github.com>
2026-06-12 23:11:14 +0900

    add workaround for loading web fonts

--- contract/wus-unit-types-v01/src/unit-helper/create-custom-element-class.ts
+++ contract/wus-unit-types-v01/src/unit-helper/create-custom-element-class.ts
@@ -1,6 +1,24 @@
+function insertLinkTagToDocumentHeadIfNotExists(url: string) {
+  const alreadyExists = Array.from(
+    document.head.querySelectorAll('link[rel="stylesheet"]'),
+  ).some((link) => (link as HTMLLinkElement).href === url);
+  if (!alreadyExists) {
+    console.log(`Inserting link tag for ${url}`);
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = url;
+    document.head.appendChild(link);
+  }
+}
+
 export function createCustomElementClass(
   fn: (shadowRoot: ShadowRoot) => () => void,
-  cssTexts: string[],
+  options: {
+    //css texts fed into a style tag in shadow dom
+    cssTexts?: string[];
+    //external stylesheet urls, e.g. web fonts, inserted into document head
+    stylesheetUrls?: string[];
+  },
 ): CustomElementConstructor {
   return class extends HTMLElement {
     isMounted: boolean;
@@ -10,14 +28,22 @@ export function createCustomElementClass(
       super();
       this.attachShadow({ mode: "open" });
       this.isMounted = false;
+
+      if (options.stylesheetUrls) {
+        options.stylesheetUrls.forEach((url) => {
+          insertLinkTagToDocumentHeadIfNotExists(url);
+        });
+      }
     }
     connectedCallback() {
       if (this.isMounted || !this.shadowRoot) return;
 
-      const style = document.createElement("style");
-      style.dataset.unit1Styles = "true";
-      style.textContent = cssTexts.join("\n");
-      this.shadowRoot.appendChild(style);
+      if (options.cssTexts) {
+        const style = document.createElement("style");
+        style.dataset.unit1Styles = "true";
+        style.textContent = options.cssTexts.join("\n");
+        this.shadowRoot.appendChild(style);
+      }
 
       this.disposeRender = fn(this.shadowRoot);
       this.isMounted = true;
