6957c356682bfb1a838843538a24e0fd32b2ee36
yahiro <yahiro07@users.noreply.github.com>
2026-06-27 23:46:46 +0900

    add thumbnail

--- unit-examples/projects/mu4-keyboard/vite.config.ts
+++ unit-examples/projects/mu4-keyboard/vite.config.ts
@@ -1,15 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   base: "./",
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({ targets: [{ src: "unit-meta.json", dest: "./" }] }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     tsconfigPaths: true,
     dedupe: ["react", "react-dom"],
